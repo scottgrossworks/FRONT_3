8fb7c80c2d3e7f76d16d77da594b2ae531770b1b
Scott Gross <scottgrossworks@gmail.com>
2023-09-08 13:13:23 -0700

    tremendous.  added user_show.html to show a user profile -- now incorporating badges.js and better badge system to display credentials for each user

---
+++ js/badges.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ * 
+ */
+
+
+const badge_1 = {
+
+    'cap': 'This is Badge #1',
+    'url': '../img/badge_1.png'
+
+}
+
+
+const badge_2 = {
+
+
+    'cap': 'This is Badge #2',
+    'url': '../img/badge_2.png'
+}
+
+
+const badge_3 = {
+
+
+    'cap': 'This is Badge #3',
+    'url': '../img/badge_3.png'
+}
+
+
+
+const badge_4 = {
+
+
+    'cap': 'This is Badge #4',
+    'url': '../img/badge_4.png'
+}
+
+
+
+const badge_5 = {
+
+
+    'cap': 'This is Badge #5',
+    'url': '../img/badge_5.png'
+}

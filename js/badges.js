c0f285bd82be8780f0d8db64defbfa4cbb91913f
Scott Gross <scottgrossworks@gmail.com>
2023-11-09 22:21:05 -0800

    getting creative with badge system

--- js/badges.js
+++ js/badges.js
@@ -4,44 +4,46 @@
  */
 
 
+
+const badge_0 = {
+
+    'tit': 'Account Secured',
+    'cap': '<a href="../leedz_about.html">Privacy for Password and Payment Info</a>',
+    'url': '../img/badge_0.png'
+};
+
+
 const badge_1 = {
 
     'tit': 'OG Founder',
-    'cap': 'One of our first sign-ups.  This user helped build the Leedz.',
+    'cap': 'Helped build the Leedz',
     'url': '../img/badge_1.png'
 
-}
+};
 
 
 const badge_2 = {
 
-    'tit': 'Don Diez',
+    'tit': 'Top Poster',
     'cap': 'Over 10 Leedz posted',
     'url': '../img/badge_2.png'
-}
+};
 
 
 const badge_3 = {
 
-    'tit': 'Midas Touch',
-    'cap': 'Over 100 Leedz posted',
+    'tit': 'Top Seller',
+    'cap': 'Over 10 Leedz sold',
     'url': '../img/badge_3.png'
-}
-
+};
 
 
 const badge_4 = {
 
-    'tit': 'Decathalete',
+    'tit': 'Top Buyer',
     'cap': 'Over 10 Leedz bought',
     'url': '../img/badge_4.png'
-}
+};
 
 
-
-const badge_5 = {
-
-    'tit': 'Centaur',
-    'cap': 'Over 100 Leedz bought',
-    'url': '../img/badge_5.png'
-}
+export const ALL_BADGES = [ badge_0, badge_1, badge_2, badge_3, badge_4 ];

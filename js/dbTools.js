903f05c3379208c0ac90dcd1532a2e5e32600b2c
Scott Gross <scottgrossworks@gmail.com>
2024-01-21 22:15:56 -0800

    cleanup and progress checking

--- js/dbTools.js
+++ js/dbTools.js
@@ -556,8 +556,6 @@ export async function db_getLeedz( subs, start_date, end_date, zip_home, zip_rad
         throwError("db_getLeedz()", "null / undefined args");
     }
 
-    console.log("db_getLeedz()");
-
     // SUBS
     // trades subscriptions will be a comma,delimited,string
     //
